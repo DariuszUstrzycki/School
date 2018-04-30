006b0470d598848edef47aac2b1eb31fd64ad300
darcoCL <*newsecond@outlook.com*>
2018-04-30 23:11:53 +0200

    Replace Form with SchoolForm in Student.java

--- src/main/java/pl/ust/school/entity/Student.java
+++ src/main/java/pl/ust/school/entity/Student.java
@@ -1,6 +1,8 @@
 package pl.ust.school.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -9,4 +11,27 @@ public class Student extends StaffPerson {
 
 	private static final long serialVersionUID = 1L;
 
+	@ManyToOne
+	@JoinColumn(name = "form_id")
+	private SchoolForm form;
+
+	/////////////// getters and setters ///////////////////
+
+	public SchoolForm getForm() {
+		return form;
+	}
+
+	public void setForm(SchoolForm form) {
+
+		if (form != null) {
+			form.getStudents().add(this);
+		} 
+		
+		if (this.form != null) { // student takes care of unlisting from the previous Form before new Form is set
+			this.form.getStudents().remove(this);
+		}
+
+		this.form = form;
+	}
+	
 }
