bf76095b952fa0137f995df90c9ca4302a2e2ba7
darcoCL <*newsecond@outlook.com*>
2018-05-01 20:17:01 +0200

    Trivial changes

--- src/main/java/pl/ust/school/repository/AppBaseRepository.java
+++ src/main/java/pl/ust/school/repository/AppBaseRepository.java
@@ -5,22 +5,30 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import pl.ust.school.entity.SchoolForm;
 
 @SuppressWarnings("hiding")
 @NoRepositoryBean                                  
 public interface AppBaseRepository<T, Long> extends Repository<T, Long> {
+	
+	// add @Query("SELECT DISTINCT .......................") ?!
 		
 	<S extends SchoolForm> S save(S entity);
 	
+	@Transactional(readOnly = true)  
 	Optional<T> findById(Long id);
 	
-	void deleteById(Long id);
-	
+	@Transactional(readOnly = true)  
 	Iterable<T> findAll();
+	
+	@Transactional
+	void deleteById(Long id);
+		
 	
 	//from PagingAndSortingRepository 
 	Iterable<T>	findAll(Sort sort);
