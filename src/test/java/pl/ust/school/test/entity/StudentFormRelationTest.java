822c91480463f3520984d75e3ab0d2af23799a44
darcoCL <*newsecond@outlook.com*>
2018-05-02 16:56:04 +0200

    Updated names in tests - a side effect of name changing in controllers $ entities

--- src/test/java/pl/ust/school/test/entity/StudentFormRelationTest.java
+++ src/test/java/pl/ust/school/test/entity/StudentFormRelationTest.java
@@ -10,15 +10,13 @@ import pl.ust.school.entity.Student;
 
 public class StudentFormRelationTest {
 	
-
-	
 	private Student getStudentWithId1AndFormId1() {
 		
 		Student student = new Student();
 		student.setId(1L);
 		SchoolForm form = new SchoolForm();
 		form.setId(1L);
-		student.setForm(form);
+		student.setSchoolForm(form);
 		
 		return student;
 	}
@@ -26,29 +24,29 @@ public class StudentFormRelationTest {
 	@Test
 	public void shouldSetFormAndIdOnStudent() {
 		assertTrue(getStudentWithId1AndFormId1().getId() == 1);
-		assertTrue(getStudentWithId1AndFormId1().getForm().getId() == 1);
+		assertTrue(getStudentWithId1AndFormId1().getSchoolForm().getId() == 1);
 	}
 	
 	@Test
 	public void shouldAddStudentInFormObject_WhenSetFormIsCalledOnStudentObject() {
 		long noOfStudentsInForm = 1;
-		assertTrue(getStudentWithId1AndFormId1().getForm().getStudents().size() == noOfStudentsInForm);
+		assertTrue(getStudentWithId1AndFormId1().getSchoolForm().getStudents().size() == noOfStudentsInForm);
 	}
 
 	@Test
 	public void shouldRemoveStudentFromPreviousForm_WhenStudentIsUpdatedWithNewForm() {
 		
 		Student studentWithFormAndId = getStudentWithId1AndFormId1();
-		SchoolForm initialForm = studentWithFormAndId.getForm();
+		SchoolForm initialForm = studentWithFormAndId.getSchoolForm();
 		
 		SchoolForm newForm = new SchoolForm();
 		long newFormId = 2L;
 		newForm.setId(newFormId);
-		studentWithFormAndId.setForm(newForm);
+		studentWithFormAndId.setSchoolForm(newForm);
 		
 		assertTrue(initialForm.getStudents().size() == 0);
 		assertTrue(newForm.getStudents().size() == 1);
-		assertTrue(studentWithFormAndId.getForm().getId() == newFormId);
+		assertTrue(studentWithFormAndId.getSchoolForm().getId() == newFormId);
 	}
 	
 	@Test
@@ -57,15 +55,15 @@ public class StudentFormRelationTest {
 		Student student = new Student();
 		SchoolForm form1 = new SchoolForm();
 		form1.setId(1L);
-		student.setForm(form1);
+		student.setSchoolForm(form1);
 		
-		assertTrue(student.getForm().getId() == 1);
+		assertTrue(student.getSchoolForm().getId() == 1);
 		assertTrue(form1.getStudents().size() == 1);
 		
 		form1.removeStudent(student);
 		
 		assertTrue(form1.getStudents().size() == 0);
-		assertNull(student.getForm());
+		assertNull(student.getSchoolForm());
 	}
 
 }
