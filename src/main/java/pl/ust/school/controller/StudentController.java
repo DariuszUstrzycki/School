e13ba2ba52f44a7798403891fd610192bd36f7b6
darcoCL <*newsecond@outlook.com*>
2018-05-04 11:15:55 +0200

    Remove unused imports form Student COntroller

--- src/main/java/pl/ust/school/controller/StudentController.java
+++ src/main/java/pl/ust/school/controller/StudentController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import java.util.Set;
 
 import javax.validation.Valid;
+import javax.validation.Validator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
