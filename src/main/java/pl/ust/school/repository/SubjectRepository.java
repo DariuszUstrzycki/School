7c26aae7081577b986782bd393307f8e1e28b002
darcoCL <*newsecond@outlook.com*>
2018-05-04 20:38:13 +0200

    Create Subject & Teacher Repositories

---
+++ src/main/java/pl/ust/school/repository/SubjectRepository.java
@@ -0,0 +1,7 @@
+package pl.ust.school.repository;
+
+import pl.ust.school.entity.Subject;
+
+public interface SubjectRepository extends AppBaseRepository<Subject, Long>{
+
+}
---
+++ src/main/java/pl/ust/school/repository/TeacherRepository.java
@@ -0,0 +1,7 @@
+package pl.ust.school.repository;
+
+import pl.ust.school.entity.Teacher;
+
+public interface TeacherRepository extends AppBaseRepository<Teacher, Long>{
+
+}
