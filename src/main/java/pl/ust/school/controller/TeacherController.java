0aab378071f4496d269b5e1aaff3fa98da74f056
darcoCL <*newsecond@outlook.com*>
2018-05-06 20:37:43 +0200

    Add CRUD functionality to Teacher Controller

--- src/main/java/pl/ust/school/controller/TeacherController.java
+++ src/main/java/pl/ust/school/controller/TeacherController.java
@@ -1,22 +1,156 @@
 package pl.ust.school.controller;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import pl.ust.school.repository.SubjectRepository;
+import pl.ust.school.entity.Teacher;
+import pl.ust.school.entity.TeacherSubject;
 import pl.ust.school.repository.TeacherRepository;
 
 @Controller
-@RequestMapping("techer") 
+@RequestMapping("teacher")
 public class TeacherController {
-	
+
 	private static final String CREATE_OR_UPDATE_FORM_VIEW = "forms/teacherForm";
 	private static final String LIST_VIEW = "detailsNLists/teacherList";
 	private static final String DETAILS_VIEW = "detailsNLists/teacherDetails";
 	private static final String CONFIRM_DELETE_VIEW = "forms/confirmDelete";
-	
+
 	@Autowired
 	private TeacherRepository teacherRepo;
+	
+	//////////////////////////////////////////////////////////
+	
+	/*
+	 *  nothing to populate the model first ?!
+	 * @ModelAttribute("schoolFormItems")
+    public Collection<SchoolForm> populateSchoolFormItems() {
+        return (Collection<SchoolForm>) this.schoolFormRepo.findAll();
+    }*/
+		
+	//////////////////////////////////////////////////////////
+	
+	@InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+	
+	////////////////////////////SAVE ////////////////////////////
+	
+	@GetMapping("/save")
+	public String showForm(Teacher teacher, Model model) {
+		model.addAttribute("entityName", "teacher"); 
+		return CREATE_OR_UPDATE_FORM_VIEW;
+	}
+	
+	@PostMapping("/save")
+	public String saveTeacher(@Valid Teacher teacher, BindingResult result, Model model) {
+		
+		model.addAttribute("entityName", "teacher"); 
+		
+		if(result.hasErrors()) {
+			List<ObjectError> list = result.getAllErrors();
+			for (ObjectError error : list) {
+				System.out.println(error);
+			}
+			return CREATE_OR_UPDATE_FORM_VIEW; 
+		}
+		
+		teacher = (Teacher) this.teacherRepo.save(teacher);	
+		return "redirect:/teacher/view/" + teacher.getId(); 
+	}
+
+	//////////////////////////// LIST ////////////////////////////
+
+	@RequestMapping("/list")
+	public String listTeachers(@RequestParam(defaultValue = "0", required = false) int min, Model model) {
+		model.addAttribute("teacherItems", this.teacherRepo.findAll());
+		model.addAttribute("entityName", "teacher");
+		return LIST_VIEW;
+	}
+
+	//////////////////////////// VIEW ONE ////////////////////////////
+	@RequestMapping("view/{id}")
+	public String viewTeacher(@PathVariable long id, Model model) {
+
+		Set<Teacher> teacherItems = new HashSet<>();
+		Optional<Teacher> opt = (Optional<Teacher>) this.teacherRepo.findById(id);
+		opt.ifPresent(teacher -> teacherItems.add(teacher));
+
+		model.addAttribute("teacherItems", teacherItems);
+		return DETAILS_VIEW;
+	}
+
+	//////////////////////////// DELETE ////////////////////////////
+
+	@GetMapping("/delete/{id}/confirm")
+	public String showConfirmationPage(@PathVariable long id, Model model) {
+		model.addAttribute("entityName", "teacher");
+		return CONFIRM_DELETE_VIEW;
+	}
+
+	@RequestMapping(value = "/delete/{id}")
+	public String deleteTeacher(@PathVariable long id) {
+
+		Optional<Teacher> opt = (Optional<Teacher>) teacherRepo.findById(id);
+		opt.ifPresent(teacher -> {
+			
+			for(TeacherSubject teachersubject : teacher.getTeacherSubjects()) {
+				teacher.removeTeacherSubject(teachersubject);
+			}
+			
+			this.teacherRepo.deleteById(id);
+
+		});
+
+		return "redirect:/teacher/list";
+	}
+
+	//////////////////////////// UPDATE ////////////////////////////
+	// Neither BindingResult nor plain target object for bean name 'teacher' available as request attribute
+	@GetMapping("/update/{id}")
+	public String showForm(@PathVariable long id, Model model) {
+		
+		Optional<Teacher> opt = (Optional<Teacher>) teacherRepo.findById(id);
+		opt.ifPresent(teacher -> {
+			model.addAttribute(teacher);
+
+		});
+		
+		return CREATE_OR_UPDATE_FORM_VIEW;
+	}
+
+	@PostMapping("/update/{id}")
+	public String updateTeacher(@Valid Teacher teacher, BindingResult result, @PathVariable long id, Model model) {
+
+		if (result.hasErrors()) {
+			return CREATE_OR_UPDATE_FORM_VIEW;
+		} else {
+			System.out.println("--------UPDATE POST: teacher: " + teacher);
+			teacher.setId(id);
+			this.teacherRepo.save(teacher);
+			System.out.println("--------UPDATE POST: after saving teacher ");
+
+			return "redirect:/teacher/view/" + id;
+		}
+		
+	}
 
 }
