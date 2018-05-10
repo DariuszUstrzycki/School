cc6992b1d401f60fe50d7364a03eb2abd6c9495b
darcoCL <*newsecond@outlook.com*>
2018-05-10 14:18:34 +0200

    Change AppBaseRepo to extend CrudRepository rather than Repository

--- src/main/java/pl/ust/school/repository/AppBaseRepository.java
+++ src/main/java/pl/ust/school/repository/AppBaseRepository.java
@@ -6,31 +6,24 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @SuppressWarnings("hiding")
 @NoRepositoryBean                                  
-public interface AppBaseRepository<T, Long> extends Repository<T , Long> {
-	
-	@Transactional
-	T save(T entity);
-	
-	@Transactional(readOnly = true)  
-	Optional<T> findById(Long id);
-	
-	@Transactional(readOnly = true)  
-	Iterable<T> findAll();
-	 
-	@Transactional
-	void deleteById(Long id);
-	
+public interface AppBaseRepository<T, Long> extends CrudRepository<T , Long> {
+
 	//from PagingAndSortingRepository 
 	@Transactional(readOnly = true)  
 	Iterable<T> findAll(Sort sort);
 	
 	@Transactional(readOnly = true) 
 	Page<T>	findAll(Pageable pageable);
+	
+
+	
+	
 	
 }
