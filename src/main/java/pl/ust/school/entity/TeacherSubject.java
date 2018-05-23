f1e3e26d06c3dbabdcc3e3ffd61d8383a364259a
darcoCL <*newsecond@outlook.com*>
2018-05-23 13:05:09 +0200

    Generate equals/hashcode for TeacherSubject

--- src/main/java/pl/ust/school/entity/TeacherSubject.java
+++ src/main/java/pl/ust/school/entity/TeacherSubject.java
@@ -63,4 +63,39 @@ public class TeacherSubject extends IdEntity {
 		this.setDeleted(true);
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((subject == null) ? 0 : subject.hashCode());
+		result = prime * result + ((teacher == null) ? 0 : teacher.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TeacherSubject other = (TeacherSubject) obj;
+		if (subject == null) {
+			if (other.subject != null)
+				return false;
+		} else if (!subject.equals(other.subject))
+			return false;
+		if (teacher == null) {
+			if (other.teacher != null)
+				return false;
+		} else if (!teacher.equals(other.teacher))
+			return false;
+		return true;
+	}
+	
+	///////////////////// equals / hashcode ////////////////////
+	
+	
+
 }
